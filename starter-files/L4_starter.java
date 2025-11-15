2bd4075d2a2d8829128842c6fa021255d21f27d1
CarlossUPC <u202321613@upc.edu.pe>
2025-11-15 04:09:14 -0500

    Create L4_starter.java

---
+++ starter-files/L4_starter.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String nombre = "Sofía";
+        int edad = 20;
+        double estatura = 1.95; 
+        double limiteAtraccion = 1.80; 
+        
+        if (edad >= 18) {
+        	System.out.println("--- RESULTADO DE EDAD ---");
+        	System.out.println("¡Eres mayor de edad!");
+        } else {
+        	System.out.println("--- RESULTADO DE EDAD ---");
+        	System.out.println("Es menor de edad.");
+        }
+
+        if (estatura > limiteAtraccion) {
+            System.out.println("--- RESULTADO DE ESTATURA ---");
+            System.out.println(nombre + " mide " + estatura + "m. ¡Puede subir a la atracción!");
+        } else {
+            System.out.println("--- RESULTADO DE ESTATURA ---");
+            System.out.println(nombre + " mide " + estatura + "m. Debe esperar a crecer más.");
+        }
+        
+        String mensajeFinal = "Alcanzó la estatura de " + estatura + " metros.";
+        System.out.println("\n" + mensajeFinal);
+        
+    }
+}
